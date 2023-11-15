ab9e30e0138c7e3cd642727911009ef8b6a0aee4
cyan-byte <62404444+cyan-byte@users.noreply.github.com>
2023-11-15 01:22:12 -0500

    email-button-bg

--- src/components/EmailButton.jsx
+++ src/components/EmailButton.jsx
@@ -2,8 +2,10 @@ import React from "react";
 
 export default function EmailButton() {
   return (
-    <button className="email-button">
-      <img src="email-icon.png" alt="mail icon" /> aliau2050@gmail.com
-    </button>
+    <div email-button-bg>
+      <button className="email-button">
+        <img src="email-icon.png" alt="mail icon" /> aliau2050@gmail.com
+      </button>
+    </div>
   );
 }
