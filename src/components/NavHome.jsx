6a243735dede219ccfc3f9e950c122950a5198a7
cyan-byte <62404444+cyan-byte@users.noreply.github.com>
2023-11-17 21:36:08 -0500

    project 3 bg

--- src/components/NavHome.jsx
+++ src/components/NavHome.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 export default function NavHome() {
   return (
@@ -9,13 +10,19 @@ export default function NavHome() {
       </Link>
       <ul>
         <li>
-          <Link to="/">home</Link>
+          <NavLink to="/" activeClassName="active-link">
+            home
+          </NavLink>
         </li>
         <li>
-          <Link to="/moreprojects">more projects</Link>
+          <NavLink to="/moreprojects" activeClassName="active-link">
+            more projects
+          </NavLink>
         </li>
         <li>
-          <Link to="/about">about</Link>
+          <NavLink to="/about" activeClassName="active-link">
+            about
+          </NavLink>
         </li>
       </ul>
     </nav>
